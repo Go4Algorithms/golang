23e1944e12db3289f2b624ff5b5c67a365c00c08
Brett-Mitchell <bm06370@georgiasouthern.edu>
2017-02-14 15:31:57 -0500

    Add IsEmpty to SimpleStack

--- DataStructures/p_SimpleStack/p_SimpleStack.go
+++ DataStructures/p_SimpleStack/p_SimpleStack.go
@@ -1,29 +1,33 @@
 package p_SimpleStack
 
 type SimpleStack struct {
-	arr []int
-	top int
+  arr []int
+  top int
 }
 
 func New() SimpleStack {
-	ss := SimpleStack{}
-	ss.arr = make([]int, 5)
-	ss.top = 0
-	return ss
+  ss := SimpleStack{}
+  ss.arr = make([]int, 5)
+  ss.top = 0
+  return ss
+}
+
+func (sq *SimpleStack) IsEmpty() bool {
+  return top == 0
 }
 
 func (ss *SimpleStack) Push(num int) {
-	if (ss.top >= len(ss.arr) - 1) {
-		ss.arr = append(ss.arr, make([]int, 5)...)
-	}
-	ss.top++
-	ss.arr[ss.top] = num
+  if (ss.top >= len(ss.arr) - 1) {
+    ss.arr = append(ss.arr, make([]int, 5)...)
+  }
+  ss.top++
+  ss.arr[ss.top] = num
 }
 
 func (ss *SimpleStack) Pop() int {
-	if (ss.top > 0) {
-		ss.top--
-		return ss.arr[ss.top + 1]
-	}
-	return 0
+  if (ss.top > 0) {
+    ss.top--
+    return ss.arr[ss.top + 1]
+  }
+  return 0
 }
