6797f4341dcea1f2457131f02ccb672ed07b3ffd
Bastiantheone <b.wieck@gmx.de>
2017-02-16 14:29:31 -0500

    Update FloydWarshall.go

--- Graph/FloydWarshall.go
+++ Graph/FloydWarshall.go
@@ -1,5 +1,8 @@
 package graph
-
+import(
+	"math"
+)
+//returns the shortest distance between each pair of nodes
 func floydWarshall(graph[][]int)[][]int {
 	var nrVertices int
 	//check if it's a for this purpose valid matrix
@@ -11,14 +14,20 @@ func floydWarshall(graph[][]int)[][]int {
 	var distance = make([][]int, nrVertices)
 	for i:=0; i<nrVertices;i++ {
 		for j:=0; j<nrVertices;j++ {
-			distance[i][j] = graph[i][j]
+			// the weighted graph matrix sets non existing edges to 0
+			if i!=j && graph[i][j]==0 {
+				distance[i][j] = math.MaxInt32
+			}
+			else{
+				distance[i][j] = graph[i][j]
+			}
 		}
 	}
 	
 	for k:=0; k<nrVertices;k++ {
 		for i:=0; i<nrVertices;i++ {
 			for j:=0; j<nrVertices;j++ {
-				if distance[i][k] + distance[k][j] < distance[i][j] {
+				if distance[i][k] + distance[k][j] < distance[i][j]{
                     			distance[i][j] = distance[i][k] + distance[k][j]
 				}
 			}
